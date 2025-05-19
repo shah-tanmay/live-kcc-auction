f2b9b403afce1b0c6f6f0eb287aef0dec98e262c
Tanmay <shahhtanmay@gmail.com>
2025-05-19 16:04:34 +0530

    team logos

--- scripts/seedData.js
+++ scripts/seedData.js
@@ -1,65 +1,65 @@
-import 'dotenv/config'; // Must be first, to load env variables
-import dotenv from 'dotenv';
+import "dotenv/config"; // Must be first, to load env variables
+import dotenv from "dotenv";
 dotenv.config();
 
-import connectToDB from '../lib/db.js';
-import Player from '../lib/models/player.js';
-import Team from '../lib/models/team.js';
-import AllPlayers from './players_seed_data.json' assert { type: 'json' };
-import Bid from '../lib/models/bid.js';
-import CuurentAuction from '../lib/models/currentauction.js';
+import connectToDB from "../lib/db.js";
+import Player from "../lib/models/player.js";
+import Team from "../lib/models/team.js";
+import AllPlayers from "./players_seed_data.json" assert { type: "json" };
+import Bid from "../lib/models/bid.js";
+import CuurentAuction from "../lib/models/currentauction.js";
 
 async function seed() {
-    await connectToDB();
+  await connectToDB();
 
-    await Player.deleteMany({});
-    await Team.deleteMany({});
-    await Bid.deleteMany({});
-    await CuurentAuction.deleteMany({});
+  await Player.deleteMany({});
+  await Team.deleteMany({});
+  await Bid.deleteMany({});
+  await CuurentAuction.deleteMany({});
 
-    await Team.create([
-        {
-            name: 'Champions -- Dream Big',
-            logoUrl: '/logos/alpha.png',
-            purseLeft: 100000,
-            squad: [],
-        },
-        {
-            name: 'Upadhyay Warriors',
-            logoUrl: '/logos/beta.png',
-            purseLeft: 100000,
-            squad: [],
-        },
-        {
-            name: 'Solanki Strikers',
-            logoUrl: '/logos/beta.png',
-            purseLeft: 100000,
-            squad: [],
-        },
-        {
-            name: 'AJ Turf Titans -- Play Bold',
-            logoUrl: '/logos/beta.png',
-            purseLeft: 100000,
-            squad: [],
-        },
-        {
-            name: 'Oswal Avengers -- Conquer the game',
-            logoUrl: '/logos/beta.png',
-            purseLeft: 100000,
-            squad: [],
-        },
-        {
-            name: 'Kumar Waghresha Smashers',
-            logoUrl: '/logos/beta.png',
-            purseLeft: 100000,
-            squad: [],
-        },
-    ]);
+  await Team.create([
+    {
+      name: "Champions -- Dream Big",
+      logoUrl: "/logos/champion.jpg",
+      purseLeft: 100000,
+      squad: [],
+    },
+    {
+      name: "Upadhyay Warriors",
+      logoUrl: "/logos/upadhyay.jpg",
+      purseLeft: 100000,
+      squad: [],
+    },
+    {
+      name: "Solanki Strikers",
+      logoUrl: "/logos/solanki.jpg",
+      purseLeft: 100000,
+      squad: [],
+    },
+    {
+      name: "AJ Turf Titans -- Play Bold",
+      logoUrl: "/logos/ajturf.jpg",
+      purseLeft: 100000,
+      squad: [],
+    },
+    {
+      name: "Oswal Avengers -- Conquer the game",
+      logoUrl: "/logos/oswal.jpg",
+      purseLeft: 100000,
+      squad: [],
+    },
+    {
+      name: "Kumar Waghresha Smashers",
+      logoUrl: "/logos/kumar.jpg",
+      purseLeft: 100000,
+      squad: [],
+    },
+  ]);
 
-    await Player.create(AllPlayers.slice(0, 10));
+  await Player.create(AllPlayers.slice(0, 10));
 
-    console.log('Seed data created!');
-    process.exit();
+  console.log("Seed data created!");
+  process.exit();
 }
 
 seed();
